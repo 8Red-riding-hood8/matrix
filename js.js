916d17f18a7e9e7cfc42cc36b9f0b416ebeb9221
8Red-riding-hood8 <denysuikmaria@gmail.com>
2024-06-02 23:32:09 +0300

    Matrix

---
+++ js.js
@@ -0,0 +1,180 @@
+let size_arr, i, j, arr = [], sum = 0, row_c, col_k;
+while ((isNaN(size_arr)) || (size_arr < 1)){
+    size_arr = +prompt("Введіть розмір масиву");
+}
+
+document.write("Згенерована функція<br/>");
+for(i = 0; i < size_arr; i++){
+    arr[i] = [];
+    for(j = 0; j < size_arr; j++){
+        arr[i][j] =  Math.round(Math.random() * 9);
+        // console.log(arr[i][j]);
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write("<br/>");
+
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i == j){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума головної діагоналі: ${sum}<br/>`);
+document.write("<br/>");
+
+sum = 0;
+for(i = size_arr - 1; 0 <= i; i--){
+    for(j = 0; j <= size_arr - 1; j++){
+        if(i == j){
+            sum += arr[i][j];
+            // console.log(arr[i][j]);
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума побочної діагоналі: ${sum}<br/>`);
+document.write("<br/>");
+
+
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i < j){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці без головної діагоналі зверху зправа: ${sum}<br/>`);
+document.write("<br/>");
+
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i <= j){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці з головної діагоналі зверху зправа: ${sum}<br/>`);
+document.write("<br/>");
+
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i > j){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці без головної діагоналі знизу зліва: ${sum}<br/>`);
+document.write("<br/>");
+
+
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i >= j){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці з головною діагоналлю знизу зліва: ${sum}<br/>`);
+document.write("<br/>");
+
+sum = 0;
+for(i = size_arr - 1; 0 <= i; i--){
+    for(j = 0; j <= size_arr - 1; j++){
+        if(i >= j){
+            sum += arr[i][j];
+            // console.log(arr[i][j]);
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці з побочною діагоналлю зверху зліва: ${sum}<br/>`);
+document.write("<br/>");
+
+
+sum = 0;
+for(i = size_arr - 1; 0 <= i; i--){
+    for(j = 0; j <= size_arr - 1; j++){
+        if(i < j){
+            sum += arr[i][j];
+            // console.log(arr[i][j]);
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума половини матриці без побічної діагоналі внизу зправа: ${sum}<br/>`);
+document.write("<br/>");
+
+while ((isNaN(row_c)) || (row_c >= size_arr)){
+    row_c = +prompt("Введіть номер стовця, суму якого ви б хотіли отримати", arr.lengthc - 1);
+}
+// console.log(row_c);
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(i == row_c){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сума строки с: ${sum}<br/>`);
+document.write("<br/>");
+
+
+while ((isNaN(col_k)) || (col_k >= size_arr)){
+    col_k = +prompt("Введіть номер стовця, суму якого ви б хотіли отримати", arr.length - 1);
+}
+// console.log(col_k);
+sum = 0;
+for(i = 0; i < size_arr; i++){
+    for(j = 0; j < size_arr; j++){
+        if(j == col_k){
+            sum += arr[i][j];
+            document.write('<span style="background-color: purple; color: white;">' + arr[i][j] + "</span> ");
+            continue;
+        }
+        document.write(arr[i][j] + " ");
+    }
+    document.write("<br/>");
+}
+document.write(`Сумма стовпця k: ${sum}<br/>`);
